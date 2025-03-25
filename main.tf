0281ba74d52ec03cadab7007895fc29ddfa6a120
maruthi2024 <158448017+maruthi2024@users.noreply.github.com>
2025-03-25 17:49:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "cicd" {
   ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0c0028e78d6709111"
-  key_name      = "your-keypair-name" # optional, but recommended
+  key_name      = key_name = "testkey" # optional, but recommended
 
   tags = {
     Name = "CICD_pipeline"
