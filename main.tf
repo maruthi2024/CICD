f54a8151d18e92c96ca0f2876566c7224cc7b7b9
maruthi2024 <158448017+maruthi2024@users.noreply.github.com>
2025-03-25 14:30:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,53 +1,18 @@
 provider "aws" {
-  region = "us-east-1" # Replace with your desired region
+  region = "us-east-1"
 }
-
-/*# Create IAM User
-resource "aws_iam_user" "kiran" {
-  name = "kiran"
-  path = "/"
-} 
-
-# Create Access Key for User
-resource "aws_iam_access_key" "kiran" {
-  user = aws_iam_user.kiran.name
-}
-
-# Attach AdministratorAccess Policy to User
-resource "aws_iam_user_policy_attachment" "kiran_admin_policy" {
-  user       = aws_iam_user.kiran.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_user_policy_attachment" "kiran_EC2_policy" {
-  user       = aws_iam_user.kiran.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-# Output Access Key and Secret Key
-output "kiran_access_key" {
-  value = aws_iam_access_key.kiran.id
-  sensitive = true
-}
-
-output "kiran_secret_key" {
-  value = aws_iam_access_key.kiran.secret
-  sensitive = true
-} 
-
-resource "aws_instance" "web" {
-  ami           = "ami-0427090fd1714168b"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0c0028e78d6709111"
-
-  tags = {
-    Name = "CICD"
-  }
-} */
 
 resource "aws_instance" "cicd" {
   ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0c0028e78d6709111"
+  subnet_id     = "subnet-0c0028e78d6709111"
+  key_name      = "your-keypair-name" # optional, but recommended
 
   tags = {
     Name = "CICD_pipeline"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.cicd.public_ip
+}
